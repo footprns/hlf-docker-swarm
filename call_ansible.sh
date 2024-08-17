259c7e7f04a7278f5d35373cff36346d79368edf
Iman Kurniawan <iman.kurniawan@vostra.co.id>
2024-08-17 23:23:06 +0700

    ca is working

--- call_ansible.sh
+++ call_ansible.sh
@@ -4,6 +4,6 @@
 cd ansible
 
 # Run the ansible playbook
-# ansible-playbook -i inventory.ini common.yml
+ansible-playbook -i inventory.ini common.yml
 # ansible-playbook -i inventory.ini orderer.yml
-ansible-playbook -i inventory.ini peer.yml
+# ansible-playbook -i inventory.ini peer.yml
